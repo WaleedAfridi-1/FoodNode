29f2322331114ceaeff6effca109a95db43952a2
Waleed Afridi <patlu7150@gmail.com>
2026-07-05 22:18:59 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,7 @@ app.use(cors({
         "https://food-node.vercel.app", 
         "http://localhost:3000"
     ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
