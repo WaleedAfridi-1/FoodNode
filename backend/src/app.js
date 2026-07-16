47c59026b6a2e8daefd75e21237b68ed299e0c7e
Waleed Afridi <patlu7150@gmail.com>
2026-07-16 17:57:13 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,18 +17,14 @@ app.use((req, res, next) => {
   console.log("ORIGIN:", req.headers.origin);
   next();
 });
-
-// 2. Manual Header Override Middleware (Yeh Browser ko shanti dega!)
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  const allowedOrigins = [
-  'http://localhost:3000', 
-  'https://food-node.vercel.app/' 
+const allowedOrigins = [
+  'http://localhost:3000', // Your local development port
+  'https://food-node.vercel.app' // Your deployed frontend URL
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
+  
     if (!origin) return callback(null, true);
     
     if (allowedOrigins.indexOf(origin) !== -1) {
@@ -37,11 +33,10 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true, // This is CRITICAL because you used credentials: 'include'
+  credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
   
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
