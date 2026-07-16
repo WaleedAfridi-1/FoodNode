d3f70a4d2f772c2afdfda8dcb35351a52074841c
Waleed Afridi <patlu7150@gmail.com>
2026-07-16 17:32:55 +0500

    Fix formatting and log message in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const app = require("./src/app");
 const ConnectDB = require("./src/db/db");
 
-
 ConnectDB();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Sever is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
